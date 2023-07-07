0291a43b3fb05a1a7fedb0bab3c4421a8a79f5ad
nicolas-chotard <115146341+nicolas-chotard@users.noreply.github.com>
2023-07-07 09:47:28 +0200

    jour2

---
+++ Jour02/Job01/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exemple de citation</title>
+	<script src="script.js"></script>
+</head>
+<body>
+	<article id="citation">
+		<p>"La vie a beaucoup plus d’imagination que nous".</p>
+	</article>
+	<button id="button">Cliquez ici</button>
+</body>
+</html>
---
+++ Jour02/Job02/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Apparition/Disparition d'un article</title>
+	<script src="script.js"></script>
+</head>
+<body>
+	<button id="button" onclick="showhide()">Cliquez ici</button>
+	<article id="article" style="display: none;">
+		<p>L'important n'est pas la chute, mais l'atterrissage.</p>
+	</article>
+</body>
+</html>
